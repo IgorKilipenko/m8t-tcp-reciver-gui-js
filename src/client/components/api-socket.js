1f1c01a8b9402399ca74a025040318f8f18b0179
IgorKilipenko <igorkili@mail.ru>
2019-03-02 21:14:37 +0700

    first commit

---
+++ src/client/components/api-socket.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+export default class ApiSocket {
+    constructor() {
+        this.instance = axios.create({
+            baseURL: `http://${REMOTE_API_URL}`, //'http://192.168.1.62',
+            timeout: 6000,
+            method: 'post',
+            //maxContentLength: 40000
+        });
+        this.headers = {
+            json: {
+                'Content-Type': 'application/json'
+            }
+        };
+        this.types = {
+            query: 'query',
+            mutation: 'mutation',
+            action: 'action'
+        };
+        this.components = {
+            receiver: 'receiver',
+            wifi: 'wifi'
+        };
+
+        console.log({baseUrl:`http://${REMOTE_API_URL}`})
+    }
+
+    getWifiList = () => {
+        return new Promise(async (reslove, reject) => {
+            try {
+                const resp = await this.instance({
+                    method: 'post',
+                    //url: '/api',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: {
+                        type: this.types.query,
+                        component: this.components.wifi,
+                        cmd: 'scan'
+                    }
+                });
+                reslove(resp);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    };
+
+    connectWiFiSTA = (ssid, password) => {
+        return new Promise(async (reslove, reject) => {
+            try {
+                const resp = await this.instance({
+                    method: 'post',
+                    url: '/service',
+                    headers: this.headers.json,
+                    //baseURL: 'http://192.168.1.62',
+                    data: {
+                        type: this.types.action,
+                        component: this.components.wifi,
+                        cmd: 'connect',
+                        ssid,
+                        password
+                    }
+                });
+                reslove(resp);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
---
+++ src/client/views/app.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import MiniDrawer from '../components/root';
+import ApiSocket from '../components/api-socket';
+import { inject, observer } from 'mobx-react';
+import { withRouter } from 'react-router-dom';
+
+const api = new ApiSocket();
+
+const styles = theme => ({});
+
+@inject('apiStore')
+@withRouter
+@observer
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            gps: {},
+            server: {}
+        };
+    }
+    sendGnssCmd = () => {
+        return new Promise(async (reslove, reject) => {
+            try {
+                const resp = await api.instance({
+                    method: 'post',
+                    url: '/api/gnss/cmd',
+                    params: this.state.gps.enabled ? { cmd: '0' } : { cmd: '1' }
+                });
+                console.log({ resp });
+                const gps = {
+                    respStatus: resp.status,
+                    data: resp.data,
+                    enabled: resp.data && resp.data.enabled
+                };
+                console.log({ gps });
+                this.setState({ gps });
+                reslove(gps);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    };
+
+    testRestApi() {
+        return new Promise(async (reslove, reject) => {
+            try {
+                const resp = await api.instance({
+                    method: 'post',
+                    url: '/rest/endpoint',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: JSON.stringify({
+                        rest_test: 'TEST SUCCESS'
+                    })
+                });
+                console.log({ resp });
+                reslove(resp);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    testApi() {
+        return new Promise(async (reslove, reject) => {
+            try {
+                const resp = await api.instance({
+                    method: 'post',
+                    //url: '/api',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: JSON.stringify({
+                        type: 'query',
+                        component: 'receiver'
+                    })
+                });
+                console.log({ resp });
+                reslove(resp);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    componentDidMount = async () => {
+        //try {
+        //    this.getGpsState();
+        //} catch (err) {
+        //    console.log({ err }, this);
+        //}
+
+        //try {
+        //    //this.testRestApi();
+        //    //this.testApi()
+        //    const res = await api.getWifiList();
+        //    console.log({res});
+        //} catch (err) {
+        //    console.log({err});
+        //}
+
+
+    };
+
+    render() {
+        const { gps } = this.state;
+        return (
+            <div>
+                <Helmet>
+                    {/*<meta charSet="ANSI" />*/}
+                    {/*<meta charSet="Windows-1252" />*/}
+                    <meta charSet="utf-8" />
+                    <title>ESP GPS</title>
+                    {/*<link rel="icon" href={favicon}/>
+                    <meta name="msapplication-TileImage" content={favicon}/>*/}
+                    <meta name="theme-color" content="#9CC2CE" />
+                    <meta
+                        name="viewport"
+                        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:300,300i,400,400i,500,500i,600,600i&amp;subset=cyrillic"
+                        rel="stylesheet"
+                    />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+                </Helmet>
+                <MiniDrawer>
+                    {/*<Button onClick={async () => this.sendGnssCmd()}>
+                        {gps.enabled ? 'Stop GPS' : 'Start GPS'}
+                    </Button>
+                    <Button>Тест utf-8</Button>*/}
+                    {this.props.children} 
+                </MiniDrawer>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(App);
