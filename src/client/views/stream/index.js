44355f392888647a691d4a4414b91cf4e06a50d8
IgorKilipenko <igorkili@mail.ru>
2019-03-11 02:25:33 +0700

    add Stream view

---
+++ src/client/views/stream/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    root: {
+        display: 'flex'
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 'auto'
+    }
+});
+
+class StreamView extends React.Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <TextField
+                id="standard-multiline-flexible"
+                label="Multiline"
+                multiline
+                rowsMax="4"
+                value="Input stream"
+                className={classes.textField}
+                margin="normal"
+                InputProps={{
+                    readOnly: true
+                }}
+            />
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(StreamView);
